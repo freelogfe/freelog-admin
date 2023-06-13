e2d929886b83f42debd9e1d815b5c3a00f0bc3a1
ZhuC <825692284@qq.com>
2023-06-13 17:31:58 +0800

    fix: 资源类型排序列表不应出现自定义类型

--- src/views/resource/type-sort.vue
+++ src/views/resource/type-sort.vue
@@ -73,7 +73,7 @@ export default {
       async getData() {
         data.tableData = [];
         data.loading = true;
-        const result = await ResourceService.getResourceTypeGroupList({ codeOrName: data.codeOrName });
+        const result = await ResourceService.getResourceTypeGroupList({ codeOrName: data.codeOrName, category: 1 });
         const { errcode } = result.data;
         if (errcode === 0) {
           dealAllPriority(result.data.data);
